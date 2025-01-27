77e69d60805aa392124207f0733b0478fbffa9a1
ths1305 <helensargent130@gmail.com>
2025-01-26 21:52:45 -0500

    Update assignment_2.py

--- assignment_2.py
+++ assignment_2.py
@@ -1,65 +1,3 @@
-
-
-#%%
-# These are helper functions for the below script
-# you do not need to do anything with this and we will cover functions later!
-#   
-# Please scroll down to the 
-# READING BEGINS HERE comment
-
-
-def displayValues(name,age,height,is_student):
-
-    # Print the variables and their types
-    print("Name:", name, " | Type:", type(name))
-    print("Age:", age, " | Type:", type(age))
-    print("Height:", height, " | Type:", type(height))
-    print("Is Student:", is_student, " | Type:", type(is_student))
-
-
-
-def dataCheck(name,age,height,is_student):
-
-    message = None
-
-    if not isinstance(name,str):
-        message = 'name is not a string'
-
-    if not isinstance(age,int):
-        message = 'age is not an int'
-
-    if not isinstance(height,float):
-        message = 'height is not a float'
-
-    if not isinstance(is_student,bool):
-        message = 'is_student is not a boolean'
-    
-    return message
-
-
-
-
-
-
-##### READING BEGINS HERE #####
-
-
-#%%
-# Declare global variables with different data types
-
-name = "Alice"           # String
-age = 25                 # Integer
-height = 5.5             # Float
-is_student = False        # Boolean
-
-
-# call function to display to screen
-displayValues(name,age,height,is_student)
-
-
-# %%
-# Variable Manipulation
-
 ##### ASSIGNMENT STARTS HERE #####
 
 '''
@@ -79,25 +17,14 @@ ASSIGNMENT GOAL:
 
 '''
 
-name = input('Please enter a name: ')
-age = input('Please enter an age: ')
-height = input('Please enter a height: ')
-is_student = input('Please enter whether or not you are a student: ')
+name = str("Helen")
+age = int(20)
+height = float(5.3)
+is_student = bool(True)
 
 
 
-displayValues(name,age,height,is_student)
+print(name,age,height,is_student)
 
 ##### ASSIGNMENT ENDS HERE #####
 
-
-#%%
-# Run this cell when you're ready to check your data!
-message = dataCheck(name,age,height,is_student)
-
-if message == None:
-    print('Data types check out - well done!')
-    print('Assignment Complete')
-
-else:
-    print(message)
