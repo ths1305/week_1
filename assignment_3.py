23738349c0f70c268db70a2d6dfd3d7d4306ea27
ths1305 <helensargent130@gmail.com>
2025-02-02 22:52:56 -0500

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -2,6 +2,8 @@
 
 
 #%%
+# FOR ALL ASSIGNMENTS: I used the end parameter so the output and label appear as a single line, 
+# For assignments 2 and 3: the 'float' is there to accommodate for decimals
 # First Assignment
 '''
 1: Please create a program which inputs two years and outputs the years as well as the difference
@@ -13,7 +15,14 @@ Year 1: 2025
 Year 2: 2028
 Difference: 3
 '''
+year1 = int(input("Enter the earlier year: "))
+year2 = int(input("Enter the later year: "))
 
+difference = (year2 - year1)
+
+print("Year 1:", year1)
+print("Year 2:", year2)
+print("Difference:", difference)
 
 #%%
 # Second Assignment
@@ -27,7 +36,13 @@ Your output should look like:
 Fahrenheit: 25
 Celsius: -3.89
 '''
+#The Celsius output is determined by input in Fahrenheit
 
+print('Fahrenheit:', end=" ")
+Fahrenheit = input()
+print('Celsius:', end=" ")
+Fahrenheit = float(Fahrenheit)
+print((Fahrenheit - 32) * 5 / 9)
 
 #%%
 # Third Assignment
@@ -42,6 +57,10 @@ USD: 1
 EU: 0.97
 
 '''
-
+print('USD:', end=" ")
+USD = input()
+print('EU:', end=" ")
+USD = float(USD)
+print(USD * 0.97)
 
 ##### ASSIGNMENT ENDS HERE #####
